b8d682f3872f91979b019b192c80674440565683
Justin Lee <justin.lee@armory.io>
2020-01-31 15:23:50 -0500

    chore(d4m): Add wrapper

---
+++ scripts/all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is a wrapper for scripts that download all.sh and run them directly
+curl -LO https://github.com/armory/minnaker/releases/download/0.0.1/minnaker.tgz
+tar -xzvf minnaker.tgz
+./minnaker/scripts/install.sh $@
