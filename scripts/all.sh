1653c3332d12b712636ce1a15d99c907725ea3c8
Justin Lee <justin.lee@armory.io>
2020-02-04 07:37:02 -0800

    chore(minnaker): Update wrapper to 0.0.2

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # This is a wrapper for scripts that download all.sh and run them directly
-curl -LO https://github.com/armory/minnaker/releases/download/0.0.1/minnaker.tgz
+curl -LO https://github.com/armory/minnaker/releases/download/0.0.2/minnaker.tgz
 tar -xzvf minnaker.tgz
 ./minnaker/scripts/install.sh $@
