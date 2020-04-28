1a6f72e514e754284ac459a4111476a2c7a05d86
Justin Lee <justin.lee@armory.io>
2020-04-28 16:26:03 -0400

    thing

--- scripts/utils/switch_to_oss.sh
+++ scripts/utils/switch_to_oss.sh
@@ -22,6 +22,19 @@ else
   yq w -i ${BASE_DIR}/manifests/halyard.yml spec.template.spec.containers[0].image gcr.io/spinnaker-marketplace/halyard:stable
 fi
 
+kubectl apply -f ${BASE_DIR}/manifests/halyard.yml
+
+sleep 5
+
+while [[ $(kubectl --context ${KUBERNETES_CONTEXT} get statefulset -n ${NAMESPACE} halyard -ojsonpath='{.status.readyReplicas}') -ne 1 ]];
+do
+  echo "Waiting for Halyard pod to start"
+  sleep 5;
+done
+
+yq r ${BASE_DIR}/.hal/config deploymentConfigurations[0].armory >> ${BASE_DIR}/halconfig_armory
+yq d -i ${BASE_DIR}/.hal/config deploymentConfigurations[0].armory
+
 VERSION=$(kubectl --context ${KUBERNETES_CONTEXT} -n ${NAMESPACE} exec -i halyard-0 -- sh -c "hal version latest -q")
 kubectl --context ${KUBERNETES_CONTEXT} -n ${NAMESPACE} exec -i halyard-0 -- sh -c "hal config version edit --version ${VERSION}"
 kubectl --context ${KUBERNETES_CONTEXT} -n ${NAMESPACE} exec -i halyard-0 -- sh -c "hal deploy apply"
