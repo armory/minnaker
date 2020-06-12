40a769718986973f093587831da601f993c3d7ff
Justin Lee <justin.lee@armory.io>
2020-06-11 21:46:46 -0400

    chore(minnaker): Add copyright statement

--- scripts/regenerate_password.sh
+++ scripts/regenerate_password.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-set -x
-set -e
+
+################################################################################
+# Copyright 2020 Armory, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
 
 # Not (currently) designed for OSX
 
@@ -9,6 +23,9 @@ set -e
 # # If a new public endpoint is provided (with the flag -P), then the new endpoint will be used
 # # Otherwise, if the previous public endpoint was provided was a flag, that endpoint will be used
 # # Otherwise, the public endpoint will be re-detected
+
+set -x
+set -e
 
 ##### Functions
 print_help () {
