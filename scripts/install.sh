014ed9198ee4f1c80df9333d10d417ad9b69e7ae
Andrew Way <andrew.way@armory.io>
2021-02-23 14:42:32 -0800

    feat(operator): Operator v2 (#77)
    * feat(operator-v2): Use spinnaker-kustomize-patches for operator installs
    Uses armory/spinnaker-kustomize-patches to install Operator by default
    although this can be overridden.
    * Added some additional input flags
    * Update refresh_endpoint to use operator
    * fix BASE_DIR
    * fix refresh_endpoint
    * adding bootstramp.yml
    * fix spin_endpoint and default pipelines
    
    Co-authored-by: Alice Chen <alice.chen@armory.io>
    Co-authored-by: Andrew Way <away@armory.io>
    Co-authored-by: Chad Tripod <chad@armory.io>

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ################################################################################
-# Copyright 2020 Armory, Inc.
+# Copyright 2021 Armory, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@
 
 # Install Minnaker in Ubuntu VM (will first install k3s)
 
-set -e
+#set -e
 
 ##### Functions
 print_help () {
@@ -27,6 +27,9 @@ print_help () {
   echo "               [-o|--oss]                                         : Install Open Source Spinnaker (instead of Armory Spinnaker)"
   echo "               [-P|--public-endpoint <PUBLIC_IP_OR_DNS_ADDRESS>]  : Specify public IP (or DNS name) for instance (rather than autodetection)"
   echo "               [-B|--base-dir <BASE_DIRECTORY>]                   : Specify root directory to use for manifests"
+  echo "               [-G|--git-spinnaker]                               : Git Spinnaker Kustomize URL (instead of https://github.com/armory/spinnaker-kustomize-patches)"
+  echo "               [--branch]                                         : Branch to clone (default 'minnaker')"
+  echo "               [-n|--nowait]                                      : Don't wait for Spinnaker to come up"
   set -x
 }
 
@@ -36,44 +39,75 @@ PROJECT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd 
 
 OPEN_SOURCE=0
 PUBLIC_ENDPOINT=""
+PUBLIC_IP=""
 MAGIC_NUMBER=cafed00d
 DEAD_MAGIC_NUMBER=cafedead
 KUBERNETES_CONTEXT=default
 NAMESPACE=spinnaker
+BASE_DIR=$PROJECT_DIR/spinsvc
+SPIN_GIT_REPO="https://github.com/armory/spinnaker-kustomize-patches"
+BRANCH=minnaker
+SPIN_WATCH=1                 # Wait for Spinnaker to come up
+OUT="$PROJECT_DIR/minnaker.log"
 
+### Load Helper Functions
+. "${PROJECT_DIR}/scripts/functions.sh"
+
+### Check if running on Mac - if so complain...
+### ToDo: Skip k3s install , and use docker-desktop or minikube context
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  echo "Use osx_install.sh to install on OSX Docker Desktop"
+  error "Use osx_install.sh to install on OSX Docker Desktop"
   exit 1
 fi
-
-BASE_DIR=/etc/spinnaker
 
 while [ "$#" -gt 0 ]; do
   case "$1" in
     -o|--oss)
-      printf "Using OSS Spinnaker"
+      info "Using OSS Spinnaker"
       OPEN_SOURCE=1
       ;;
     -x)
-      printf "Excluding from Minnaker metrics"
+      info "Excluding from Minnaker metrics"
       MAGIC_NUMBER=${DEAD_MAGIC_NUMBER}
       ;;
     -P|--public-endpoint)
-      if [ -n $2 ]; then
-        PUBLIC_ENDPOINT=$2
+      if [[ -n $2 ]]; then
+        PUBLIC_IP=$2
         shift
       else
-        printf "Error: --public-endpoint requires an IP address >&2"
+        error "--public-endpoint requires an IP address >&2"
         exit 1
       fi
       ;;
     -B|--base-dir)
-      if [ -n $2 ]; then
+      if [[ -n $2 ]]; then
         BASE_DIR=$2
+        warn "Contents in $2 will be erased"
       else
-        printf "Error: --base-dir requires a directory >&2"
+        error "--base-dir requires a directory >&2"
         exit 1
       fi
+      ;;
+    -G|--git-spinnaker)
+      if [[ -n $2 ]]; then
+        SPIN_GIT_REPO=$2
+        BRANCH=master
+      else
+        error "--git-spinnaker requires a git url >&2"
+        exit 1
+      fi
+      ;;
+    --branch)
+      if [[ -n $2 ]]; then
+        BRANCH=$2
+      else
+        info "Defaulting to branch 'minnaker' for $SPIN_GIT_REPO"
+        BRANCH=minnaker
+      fi
+      ;;
+    -n|--nowait)
+      info "Will not wait for Spinnaker to come up"
+      SPIN_WATCH=0
       ;;
     -h|--help)
       print_help
@@ -83,84 +117,58 @@ while [ "$#" -gt 0 ]; do
   shift
 done
 
-. ${PROJECT_DIR}/scripts/functions.sh
-
-if [[ ${OPEN_SOURCE} -eq 1 ]]; then
-  printf "Using OSS Spinnaker"
-  HALYARD_IMAGE="us-docker.pkg.dev/spinnaker-community/docker/halyard:stable"
+if [[ ${OPEN_SOURCE} == 1 ]]; then
+  info "Using OSS Spinnaker"
+  SPIN_FLAVOR=oss
+  VERSION=$(curl -s https://spinnaker.io/community/releases/versions/ | grep 'id="version-' | head -1 | sed -e 's/\(<[^<][^<]*>\)//g; /^$/d' | cut -d' ' -f2)
 else
-  printf "Using Armory Spinnaker"
-  # This is defined in functions.sh
-  HALYARD_IMAGE="${ARMORY_HALYARD_IMAGE}"
+  info "Using Armory Spinnaker"
+  SPIN_FLAVOR=armory
+  VERSION=$(curl -sL https://halconfig.s3-us-west-2.amazonaws.com/versions.yml | grep 'version: ' | awk '{print $NF}' | sort | tail -1)
 fi
 
-echo "Setting the Halyard Image to ${HALYARD_IMAGE}"
+info "Running minnaker setup for Linux"
+info "Cloning repo: ${SPIN_GIT_REPO}#${BRANCH} into ${BASE_DIR}"
 
-echo "Running minnaker setup for Linux"
-  
-# Scaffold out directories
-# OSS Halyard uses 1000; we're using 1000 for everything
-sudo mkdir -p ${BASE_DIR}/templates/{manifests,profiles,service-settings}
-sudo mkdir -p ${BASE_DIR}/manifests
-sudo mkdir -p ${BASE_DIR}/.kube
-sudo mkdir -p ${BASE_DIR}/.hal/.secret
-sudo mkdir -p ${BASE_DIR}/.hal/default/{profiles,service-settings}
+if [ -d "${BASE_DIR}" ]; then
+  warn "${BASE_DIR} exists already.  FOLDER CONTENTS WILL GET OVERWRITTEN!"
+  warn "PROCEEDING in 3 secs... (ctrl-C to cancel; use -B option to specify a different directory)"
+  sleep 3
+fi
+rm -rf ${BASE_DIR}
+git clone -b ${BRANCH} "${SPIN_GIT_REPO}" "${BASE_DIR}"
+cd "${BASE_DIR}"
 
-sudo chown -R 1000 ${BASE_DIR}
+### Installing helper tools
+install_yq
+install_jq
 
 detect_endpoint
 generate_passwords
-copy_templates
-update_templates_for_auth
+update_endpoint
 hydrate_templates
-conditional_copy
-
-### Set up Kubernetes environment
-echo "Installing K3s"
-install_k3s
-echo "Setting kubernetes context to Spinnaker namespace"
-sudo env "PATH=$PATH" kubectl config set-context ${KUBERNETES_CONTEXT} --namespace ${NAMESPACE}
-echo "Installing yq"
-install_yq
-
-### Create all manifests:
-# - namespace - must be created first
-# - halyard
-# - minio
-# - clusteradmin
-# - ingress
-echo "Creating manifests"
-kubectl --context ${KUBERNETES_CONTEXT} apply -f ${BASE_DIR}/manifests/namespace.yml
-kubectl --context ${KUBERNETES_CONTEXT} apply -f ${BASE_DIR}/manifests
-  
-######## Bootstrap
-while [[ $(kubectl --context ${KUBERNETES_CONTEXT} get statefulset -n ${NAMESPACE} halyard -ojsonpath='{.status.readyReplicas}') -ne 1 ]];
-do
-  echo "Waiting for Halyard pod to start"
-  sleep 5;
-done
-
-sleep 5;
-create_hal_shortcut
 create_spin_endpoint
 
-VERSION=$(kubectl --context ${KUBERNETES_CONTEXT} -n ${NAMESPACE} exec -i halyard-0 -- sh -c "hal version latest -q")
-kubectl --context ${KUBERNETES_CONTEXT} -n ${NAMESPACE} exec -i halyard-0 -- sh -c "hal config version edit --version ${VERSION}"
-kubectl --context ${KUBERNETES_CONTEXT} -n ${NAMESPACE} exec -i halyard-0 -- sh -c "hal deploy apply"
+### Set up Kubernetes environment
+install_k3s
+info "Setting Kubernetes context to Spinnaker namespace"
+sudo env "PATH=$PATH" kubectl config set-context ${KUBERNETES_CONTEXT} --namespace ${NAMESPACE}
 
-spin_endpoint
+### Deploy Spinnaker with Spinnaker Operator
+cd "${BASE_DIR}"
+SPIN_FLAVOR=${SPIN_FLAVOR} SPIN_WATCH=0 ./deploy.sh
 
-while [[ $(kubectl -n ${NAMESPACE} get pods --field-selector status.phase!=Running 2> /dev/null | wc -l) -ne 0 ]];
-do
-  echo "Waiting for all containers to be Running"
-  kubectl -n ${NAMESPACE} get pods
-  sleep 5
-done
+# Install PACRD
+exec_kubectl_mutating "kubectl apply -f https://engineering.armory.io/manifests/pacrd-1.0.1.yaml -n spinnaker" handle_generic_kubectl_error
+exec_kubectl_mutating "kubectl apply -k ${PROJECT_DIR}/pipelines -n spinnaker" handle_generic_kubectl_error
 
-kubectl -n ${NAMESPACE} get pods
-
+echo '' >>~/.bashrc                                     # need to add empty line in case file doesn't end in newline
 echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
 
-set +x
-echo "It may take up to 10 minutes for this endpoint to work.  You can check by looking at running pods: 'kubectl -n ${NAMESPACE} get pods'"
 spin_endpoint
+
+if [[ ${SPIN_WATCH} != 0 ]]; then
+  watch kubectl get pods,spinsvc -n spinnaker
+fi
